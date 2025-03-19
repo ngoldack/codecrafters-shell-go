da3c77a9066e2c8a1e88ddd7d5749ac6dbd7009f
Nicolas Goldack <nicolas.goldack@arag.de>
2025-03-19 10:42:37 +0100

    test

--- app/main.go
+++ app/main.go
@@ -11,14 +11,10 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	for {
-		i, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			panic(err)
-		}
-
-		if i != "" {
-			break
-		}
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		panic(err)
 	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
