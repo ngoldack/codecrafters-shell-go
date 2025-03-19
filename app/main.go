4bd98a3b544bbd40ac161c3c50853dab35b68036
Nicolas Goldack <nicolas.goldack@arag.de>
2025-03-19 11:01:37 +0100

    test

--- app/main.go
+++ app/main.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+// args index 0 is always the command itself
+type CommandFunc = func(args []string) error
+
+var cmds map[string]CommandFunc = map[string]CommandFunc{
+	"exit": func(args []string) error {
+		if len(args) <= 1 {
+			return errors.New("no status code")
+		}
+
+		i, err := strconv.Atoi(args[1])
+		if err != nil {
+			return fmt.Errorf("failed to convert to int: %w", err)
+		}
+
+		os.Exit(i)
+		return nil
+	},
+}
 
 func main() {
 	for {
@@ -16,6 +38,30 @@ func main() {
 			panic(err)
 		}
 
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		args := strings.Split(command, " ")
+
+		cmd, err := getCommand(args)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		err = cmd(args)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
 	}
+}
+
+func getCommand(command []string) (CommandFunc, error) {
+	if len(command) == 0 {
+		return nil, errors.New("empty command")
+	}
+
+	cmd, ok := cmds[command[0]]
+	if !ok {
+		return nil, fmt.Errorf("%s: command not found", command[:len(command)-1])
+	}
+
+	return cmd, nil
 }
